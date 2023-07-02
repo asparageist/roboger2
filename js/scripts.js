5084a0cc4c8410795bb8f9a485307bf8ff4447e8
Joseph Murray <josephwmurray@gmail.com>
2023-07-02 08:55:03 -0700

    almost complete 4th TDD test, including multiple digit numbers into the query. Ran out of time. WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,33 @@
 window.onload = function() {
-  // document.querySelector("form#input").addEventListener("submit", count);
+  // document.querySelector("form#input").addEventListener("submit", makeArray());
 };
 
 const numbo = 13;
 const numArray = [];
 const beepArray = [];
 
-function count() {
+function makeArray() {
   if (numbo >= 0) {
     for (i = 0; i <= numbo; i++) {
       numArray.push(i);
     }
   }
   console.log(numArray);
-  digits();
+  isBeepBoop();
 }
 
-function isBeepBoop(num) {
-  for (i = 0; i <= num; i++) {
-    if (numArray[i] === 1) {
+function isBeepBoop() {
+  for (i = 0; i < numArray.length; i++) {
+    let digit = numArray[i].toString();
+    if (digit.includes("1")){
       beepArray.push("BEEP!");
-    } else if (numArray[i] === 2) { 
+    } else if (digit.includes("2")) { 
       beepArray.push("BOOP!");
-    } else if (numArray[i] === 3) {
+    } else if (digit.includes("3")) {
       beepArray.push("WONT YOU BE MY NEIGHBOR");
     } else {
-      beepArray.push(numArray[i]);
+      beepArray.push(digit);
     }
   }
-}
-
-function digits() {
-  for (i = 0; i <= numbo; i++) {
-    let numDigit = String(i);
-    for (ii = 0; ii <= numDigit.length; ii++) {
-      isBeepBoop(ii);
-    }
-  }
+  console.log(beepArray);
 }
